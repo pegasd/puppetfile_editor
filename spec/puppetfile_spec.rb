eb1538759f0dd60d56473ee4d031a06084c9872f
Eugene Piven <thepegasd@gmail.com>
2017-09-19 22:57:18 +0300

    Update fixtures Puppetfile and related test

--- spec/puppetfile_spec.rb
+++ spec/puppetfile_spec.rb
@@ -11,22 +11,22 @@ RSpec.describe PuppetfileEditor::Puppetfile do
 
   describe '#load' do
     it 'loads basic Puppetfile' do
-      pedit = described_class.new(File.join fixtures_dir, 'Puppetfile')
+      pedit = described_class.new(File.join(fixtures_dir, 'Puppetfile'))
       pedit.load
       expect(pedit.modules).to be_kind_of(Array)
     end
 
     it 'fails when file does not exist' do
-      pedit = described_class.new(File.join fixtures_dir,'nonexistant', 'Puppetfile')
+      pedit = described_class.new(File.join(fixtures_dir, 'nonexistant', 'Puppetfile'))
       expect { pedit.load }.to raise_error(StandardError, /missing or unreadable/)
     end
   end
 
   describe '#generate_puppetfile' do
     it 'outputs Puppetfile' do
-      pedit = described_class.new(File.join fixtures_dir, 'Puppetfile')
+      pedit = described_class.new(File.join(fixtures_dir, 'Puppetfile'))
       pedit.load
-      expect(pedit.generate_puppetfile).to eq(<<~EOS
+      expect(pedit.generate_puppetfile).to eq(<<~RUBY
         # Local modules
         mod 'config', :local
 
@@ -42,14 +42,14 @@ RSpec.describe PuppetfileEditor::Puppetfile do
         mod 'nginx',
             git: 'https://github.com/voxpupuli/puppet-nginx',
             tag: :latest
-      EOS
+      RUBY
       )
     end
 
     it 'reformats Puppetfile properly' do
-      pedit = described_class.new(File.join fixtures_dir, 'unformatted', 'Puppetfile')
+      pedit = described_class.new(File.join(fixtures_dir, 'unformatted', 'Puppetfile'))
       pedit.load
-      expect(pedit.generate_puppetfile).to eq(<<~EOS
+      expect(pedit.generate_puppetfile).to eq(<<~RUBY
         # Local modules
         mod 'celery', :local
         mod 'check_geoip', :local
@@ -57,31 +57,35 @@ RSpec.describe PuppetfileEditor::Puppetfile do
         mod 'kibastic', :local
         mod 'lsyncd', :local
         mod 'mongo', :local
-        mod 'nginx', :local
         mod 's3cmd', :local
         mod 'vcsrepo', :local
         mod 'vpn', :local
 
         # Modules from the Puppet Forge
-        mod 'puppetlabs/concat', '4.0.1'
-        mod 'puppetlabs/stdlib', '4.17.1'
-        mod 'puppetlabs/lvm', '0.7.0'
-        mod 'puppetlabs/inifile', '1.4.3'
+        mod 'KyleAnderson/consul', '2.1.0'
+        mod 'camptocamp/kmod', '2.1.0'
+        mod 'locp/cassandra', '1.13.0'
+        mod 'petems/swap_file', '3.0.2'
         mod 'puppetlabs/apache', '1.10.0'
+        mod 'puppetlabs/concat', '4.0.1'
+        mod 'puppetlabs/inifile', '1.4.3'
+        mod 'puppetlabs/lvm', '0.7.0'
         mod 'puppetlabs/postgresql', '5.0.0'
         mod 'puppetlabs/rabbitmq', '5.6.0'
-        mod 'locp/cassandra', '1.13.0'
+        mod 'puppetlabs/stdlib', '4.17.1'
         mod 'wywygmbh/fluentd', '0.5.0'
-        mod 'camptocamp/kmod', '2.1.0'
-        mod 'petems/swap_file', '3.0.2'
-        mod 'KyleAnderson/consul', '2.1.0'
-      EOS
+
+        # Git modules
+        mod 'nginx',
+            git: 'https://github.com/voxpupuli/puppet-nginx',
+            tag: :latest
+      RUBY
       )
     end
   end
 
   it 'properly fails when parsing broken Puppetfile' do
-    pedit = described_class.new(File.join fixtures_dir, 'broken', 'Puppetfile')
+    pedit = described_class.new(File.join(fixtures_dir, 'broken', 'Puppetfile'))
     expect { pedit.load }
       .to raise_error(NoMethodError, /unrecognized declaration 'omg'/)
   end
