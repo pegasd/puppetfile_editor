94b8bc6fb3817a4e543d403bd860a18e58451501
Eugene Piven <thepegasd@gmail.com>
2018-03-11 21:10:15 +0300

    Enable coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,10 @@
 # frozen_string_literal: true
 
+require 'coveralls'
+Coveralls.wear!
+
 require 'bundler/setup'
 require 'puppetfile_editor'
-
-RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
-end
 
 # Various spec helper methods
 class SpecHelper
